042dcc9a36419687a285ca5d2f1df6005e902a7c
Djefrid Byli Fotue Kuate <2210038@etu.cegepjonquiere.ca>
2024-02-29 09:22:53 -0500

    labo fini

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "250px";
+function ListPublication() {
+  fetch('http://localhost:3000/Publication')
+    .then(response => response.json())
+    .then(json => remplirePublication(json))
+    .catch(err => console.log(err));
+}
+
+
+
+function ListCommentaire() {
+  fetch('http://localhost:3000/Commentaire')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+}
+ListCommentaire();
+
+
+
+// remplir le contenu
+function remplirePublication(ListPublication) {
+
+  for (let i = 0; i < ListPublication.length; i++) {
+    let design = `<div class="col-10 col-lg-3 bloc">
+                    <a class="text-decoration-none" href="index.html?id=${ListPublication[i].id}">
+                        <div class="row">
+                            <div class="card rounded-4 border border-3 border-secondary hover-zoom mx-auto">
+                                <img class="card-img-top image" src="paysage.jpg" alt="Card image cap">
+                                <div class="fs-4 p-3 mb-2 bg-secondary text-white card-title" id="titre">${ListPublication[i].Titre}</div>
+                                <div class="card-body">
+                                    <p class="card-text contenue">${ListPublication[i].Contenu}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                  </div>`;
+
+
+
+                 
+    document.getElementById("ContenuPublication").innerHTML += design;
+    trimText(document.querySelectorAll(".card-text")[i],50);
+    trimText(document.querySelectorAll(".card-title")[i],15); 
   }
-  
-  /* Set the width of the sidebar to 0 (hide it) */
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
+}
+
+ListPublication();
+
+// trimtext carbody
+
+function trimText( card ,maxlength) {
+  if(card.textContent.length > maxlength) 
+    card.textContent = card.textContent.substring(0, maxlength)+'...';
+}
+
+
+// executer une suite de fonction pour le traitement devenement sur le bouton
+
+
+
+
+
+
